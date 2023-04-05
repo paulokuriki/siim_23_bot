f97b49780931b8831f1d71300bfc66363cbef1c9
paulokuriki <paulokuriki@gmail.com>
2023-04-04 21:53:29 -0300

    connecting to Postgres

---
+++ database.py
@@ -0,0 +1,57 @@
+import os
+from sqlalchemy import create_engine,  select, text
+from sqlalchemy import MetaData
+from sqlalchemy import Table, Column
+from sqlalchemy import DateTime as TimeStamp, SmallInteger as smallint, Text as text, REAL as real, BOOLEAN as boolean
+
+metadata_obj = MetaData()
+
+competitors = Table("competitors", metadata_obj,
+                    Column("id", text, primary_key=True),
+                    Column("username", text))
+
+pretrained = Table("pretrained_results", metadata_obj,
+                   Column("id", smallint, primary_key=True),
+                   Column("timestamp", TimeStamp),
+                   Column("trainer", text),
+                   Column("gpu", text),
+                   Column("training_secs", smallint),
+                   Column("model_name", text),
+                   Column("batch_size", smallint),
+                   Column("epochs", smallint),
+                   Column("learning_rate", real),
+                   Column("batch_norm", boolean),
+                   Column("filters", smallint),
+                   Column("dropout", real),
+                   Column("image_size", smallint),
+                   Column("metrics_train_set", real),
+                   Column("metrics_val_set", real),
+                   Column("metrics_test_set", real))
+
+
+DATABASE_URL = os.environ.get('DATABASE_URL', '')
+
+engine = create_engine(DATABASE_URL)
+conn = engine.connect()
+
+def list_users(id: str = None):
+
+    if id:
+        sql = select(competitors.c.id).where(competitors.c.id == id)
+    else:
+        sql = select(competitors.c.id)
+
+    results = conn.execute(sql) .fetchall()
+
+    return results
+
+
+def list_pretrained(id: str = None):
+    if id:
+        sql = select(pretrained).where(pretrained.c.id == id)
+    else:
+        sql = select(pretrained)
+
+    results = conn.execute(sql).fetchall()
+
+    return results
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 #https://www.pragnakalp.com/create-telegram-bot-using-python-tutorial-with-examples/
 
-import json
-import os
-
-import requests
 from flask import Flask, request, Response
+
+from telegram_aux import *
+from database import *
 
 # token that we get from the BotFather
 TOKEN = os.environ.get('TELEGRAM_TOKEN', '')
@@ -22,7 +21,8 @@ def index():
                 tel_send_message(chat_id, f"Hello, {username}! Welcome to the SIIM 2023 AI Playground")
                 tel_send_inlinebutton(chat_id, "Select your option:",
                                       [{"text": "Create new model", "callback_data": "new_model"},
-                                       {"text": "Check Status", "callback_data": "check_status"}])
+                                       {"text": "Check Status", "callback_data": "check_status"},
+                                       {"text": "List Users", "callback_data": "list_users"}])
             elif txt == "new_model":
                 tel_send_message(chat_id, f"Creating a new model for user {username}.")
                 tel_send_inlinebutton(chat_id, "Select your architecture:",
@@ -38,6 +38,8 @@ def index():
                 tel_send_message(chat_id, f"Training your model for: {txt}.")
             elif txt == "check_status":
                 tel_send_message(chat_id, f"{username}, here is your model's status: Train Loss: 6.3  Val Loss: 8.6 ROC AUC: 0.89")
+            elif txt == "list_users":
+                tel_send_message(chat_id, list_users())
             elif txt == "image":
                 tel_send_image(chat_id)
             elif txt == "poll":
@@ -66,155 +68,6 @@ def index():
         return "<h1>Welcome!</h1>"
 
 
-# Reading the JSON format when we send the text message and extracting the chat id of the user and the text that user send to the bot
-def tel_parse_message(message):
-    print("message-->", message)
-    try:
-
-        if message.get('message', None):
-            chat_id = message['message']['chat']['id']
-            txt = message['message']['text']
-            username = message['message']['from']['username']
-        elif message.get('callback_query', None):
-            chat_id = message['callback_query']['from']['id']
-            txt = message['callback_query']['data']
-            username = message['callback_query']['from']['username']
-        else:
-            return '', '', ''
-
-        print("chat_id-->", chat_id)
-        print("txt-->", txt)
-        print("username-->", username)
-
-        return chat_id, txt, username
-    except:
-        print("No text found-->>")
-
-
-# Get the Text message response from the bot
-def tel_send_message(chat_id, text):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    payload = {
-        'chat_id': chat_id,
-        'text': text
-    }
-    r = requests.post(url, json=payload)
-    return r
-
-
-# Get the Image response from the bot by providing the image link
-def tel_send_image(chat_id):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
-    payload = {
-        'chat_id': chat_id,
-        'photo': "https://raw.githubusercontent.com/fbsamples/original-coast-clothing/main/public/styles/male-work.jpg"
-    }
-    r = requests.post(url, json=payload)
-    return r
-
-
-# Get the Poll response from the bot
-def tel_send_poll(chat_id):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendPoll'
-    payload = {
-        'chat_id': chat_id,
-        "question": "In which direction does the sun rise?",
-        # options are provided in json format
-        "options": json.dumps(["North", "South", "East", "West"]),
-        "is_anonymous": False,
-        "type": "quiz",
-        # Here we are providing the index for the correct option(i.e. indexing starts from 0)
-        "correct_option_id": 2
-    }
-    r = requests.post(url, json=payload)
-    return r
-
-
-# Get the Button response in the keyboard section
-def tel_send_button(chat_id):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-
-    payload = {
-        'chat_id': chat_id,
-        'text': "What is this?",  # button should be in the propper format as described
-        'reply_markup': {
-            'keyboard': [[
-                {
-                    'text': 'supa'
-                },
-                {
-                    'text': 'mario'
-                }
-            ]]
-        }
-    }
-    r = requests.post(url, json=payload)
-    return r
-
-
-# Get the Inline button response
-def tel_send_inlinebutton(chat_id, message, options):
-    '''
-
-    :param chat_id: parameter received from Telegram
-    :param message: Header message
-    :param options: eg [{"text": "A", "callback_data": "ic_A"}, {"text": "B", "callback_data": "ic_B"}]
-    :return: the request response
-    '''
-
-    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-
-    payload = {
-        'chat_id': chat_id,
-        'text': message,
-        'reply_markup': {"inline_keyboard": [options]}
-    }
-    r = requests.post(url, json=payload)
-    return r
-
-
-# Get the Button response from the bot with the redirected URL
-def tel_send_inlineurl(chat_id):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-
-    payload = {
-        'chat_id': chat_id,
-        'text': "Which link would you like to visit?",
-        'reply_markup': {
-            "inline_keyboard": [
-                [
-                    {"text": "google", "url": "http://www.google.com/"},
-                    {"text": "youtube", "url": "http://www.youtube.com/"}
-                ]
-            ]
-        }
-    }
-    r = requests.post(url, json=payload)
-    return r
-
-
-# Get the Audio response from the bot by providing the URL for the audio
-def tel_send_audio(chat_id):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendAudio'
-
-    payload = {
-        'chat_id': chat_id,
-        "audio": "http://www.largesound.com/ashborytour/sound/brobob.mp3",
-    }
-    r = requests.post(url, json=payload)
-    return r
-
-
-# Get the Document response from the bot by providing the URL for the Document
-def tel_send_document(chat_id):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendDocument'
-
-    payload = {
-        'chat_id': chat_id,
-        "document": "http://www.africau.edu/images/default/sample.pdf",
-    }
-    r = requests.post(url, json=payload)
-    return r
 
 
 if __name__ == '__main__':
