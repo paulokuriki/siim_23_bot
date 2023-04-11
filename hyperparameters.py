fab2a7aa6fac5be37c9e7768ba29c1324c62d7bc
paulokuriki <paulokuriki@gmail.com>
2023-04-10 22:58:03 -0300

    changes to hyperparameters

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,14 +1,13 @@
-batch_sizes = ['bs_4', 'bs_8', 'bs_16', 'bs_32', 'bs_64', 'bs_128']
+batch_sizes = ['bs_2', 'bs_8', 'bs_32']
 
-epochs = ['epochs_1', 'epochs_3', 'epochs_5', 'epochs_7', 'epochs_10', 'epochs_15', 'epochs_20', 'epochs_25',
-          'epochs_30', 'epochs_40', 'epochs_50']
+epochs = ['epochs_5', 'epochs_10', 'epochs_30']
 
-learning_rates = ['lr_0.1', 'lr_0.01', 'lr_0.001', 'lr_0.0001', 'lr_0.00001']
+learning_rates = ['lr_0.001', 'lr_0.0001', 'lr_0.00001']
 
-batch_norm = ['bn_true', 'bn_false']
+batch_norm = ['bn_True', 'bn_False']
 
-filters = ['filters_5', 'filters_10', 'filters_15', 'filters_20']
+filters = ['filters_8', 'filters_32']
 
-dropout = ['drop_none', 'drop_0.1', 'drop_0.2', 'drop_0.3', 'drop_0.5', 'drop_0.75', 'drop_0.9']
+dropout = ['drop_None', 'drop_0.2']
 
-image_size = ['imgsize_16', 'imgsize_32', 'imgsize_64', 'imgsize_128', 'imgsize_256', 'imgsize_512']
+image_size = ['imgsize_64', 'imgsize_128', 'imgsize_256']
