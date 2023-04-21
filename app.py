ed401d2c0015f26c4bac27fc221f2154dff910c7
paulokuriki <paulokuriki@gmail.com>
2023-04-21 20:43:02 -0300

    Revert "included a monitor iframe from https://stats.uptimerobot.com/KVqA7IDVgq"
    This reverts commit fefa2f07f29328657694d69a39f3a1695531e5be.

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 # Import necessary modules
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
-import aiohttp
 
 from messages import *
 # Import functions from other modules
@@ -83,12 +82,9 @@ async def index(request: Request):
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    uptime_robot_page = await fetch_uptime_robot_page()
-    return HTMLResponse(f"""
-        <h1>Welcome to the SIIM AI Playground API!</h1>
-        <p>Open Telegram and look for the bot called @siim_23_bot to start to play.</p>
-        <iframe srcdoc="{uptime_robot_page}" width="100%" height="600px" frameborder="0"></iframe>
-    """, status_code=200)
+
+    return HTMLResponse("<h1>Welcome to the SIIM AI Playground API!</h1>" \
+           "<p>Open Telegram and look for the bot called @siim_23_bot to start to play.", status_code=200)
 
 
 # Route for uploading JSON records to the database
@@ -141,8 +137,3 @@ def list_competitors():
     return JSONResponse(results, status_code=200)
 
 
-async def fetch_uptime_robot_page():
-    url = "https://stats.uptimerobot.com/KVqA7IDVgq"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            return await response.text()
